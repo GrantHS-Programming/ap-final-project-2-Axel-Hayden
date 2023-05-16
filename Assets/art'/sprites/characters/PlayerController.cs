69b0b1816bcbe717b0988887486b46bad8e0fcd5
ahayden4792 <ahayden4792@student.pps.net>
2023-05-16 13:45:29 -0700

    wewe woowoo

--- Assets/art'/sprites/characters/PlayerController.cs
+++ Assets/art'/sprites/characters/PlayerController.cs
@@ -8,19 +8,21 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 moveDirection;
-    
+
     void Start()
     {
 
     }
 
-    void Update() {
+    void Update()
+    {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
-    
+
 }
