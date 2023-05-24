a9ce601ed111d2b4b0d98a0140dbb01074681f92
ahayden4792 <ahayden4792@student.pps.net>
2023-05-24 13:43:30 -0700

    progress

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour{
     public float speed = 4f;
+    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] private float attackSpeed = 1f;
+    private float canAttack;
     private Transform target;
 
     private void Update(){
@@ -13,11 +16,22 @@ public class enemy : MonoBehaviour{
         }
     }
 
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            if (attackSpeed <= canAttack)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                canAttack = 0f;
+            }else {
+                canAttack += Time.deltaTime;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
             target = other.transform;
-
-            
         }
     }
     private void OnTriggerExit2D(Collider2D other)
@@ -25,8 +39,6 @@ public class enemy : MonoBehaviour{
         if (other.gameObject.tag == "Player")
         {
             target = null;
-
-
         }
     }
 }
