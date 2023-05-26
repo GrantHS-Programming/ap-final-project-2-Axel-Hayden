e8c933f5f45fa6f65132bb131cd3c2db44d3e0b0
ahayden4792 <ahayden4792@student.pps.net>
2023-05-26 13:59:30 -0700

    shit wacky

--- Assets/art'/sprites/characters/PlayerController.cs
+++ Assets/art'/sprites/characters/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 moveDirection;
+    [SerializeField] Transform hand;
+    
 
     void Start()
     {
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        RotateHand();
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
@@ -23,6 +26,12 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void RotateHand() {
+        float angle = Utility.AngleTowardsMouse(hand.position);
+        hand.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    
     }
 
 }
